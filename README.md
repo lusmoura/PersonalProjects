4d153f4184f34e7966a84472daa49d83329f2fae
lusmoura <luisasouzamoura@gmail.com>
2019-03-15 22:26:40 -0300

    updating readme

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Just some small codes I make with no particular reason\
 
 -Is this prime: a game to learn the prime numbers. Based on an online game with the same name.\
 -OpenCV: useful codes to deal with images 
+-Data Science: some codes I made while studying data science and machine learning
--- openCV/resize.py
+++ openCV/resize.py
@@ -7,14 +7,16 @@ def main():
 
 	print("Type the name of the image:")
 	name = input()
-
+	print("Type the scale (%): ")
+	scale = input()
+	
 	try:
 		img = cv2.imread(name)
 		
-		aux = 100.0 / img.shape[1]
-		factor = (100, int(img.shape[0] * aux))
-	
-		resized = cv2.resize(img, factor, interpolation= cv2.INTER_AREA)
+		width = int(img.shape[1] * int(scale) / 100)
+		height = int(img.shape[0] * int(scale) / 100)
+		
+		resized = cv2.resize(img, (width, height), interpolation= cv2.INTER_AREA)
 		cv2.imwrite('resized.jpg', resized)
 	except:
 		print("Something went wrong")
